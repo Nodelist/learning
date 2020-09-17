0605f5234b963a32da57a16dd705aba7dbf41847
wq <2359188347@qq.com>
2020-09-17 18:08:36 +0800

    新增笔试笔试题

--- es6-test/src/interview.js
+++ es6-test/src/interview.js
@@ -73,3 +73,53 @@ console.log(1 + true) // 2
 console.log(1 + false) // 1
 console.log('' + true) // true
 console.log('' + false) // false
+
+
+/**
+ * @description 求 k 的 m 进制数中含有多少个 n
+ * @param {*} k 
+ * @param {*} n 
+ * @param {*} m 
+ */
+function returnCount(k, n, m) {
+    var count = 0
+    var km = k.toString(m);
+    for (var i = 0; i < km.length; i++) {
+        if (km[i] == n) {
+            count += 1
+        }
+    }
+    return count;
+}
+// console.log(returnCount(24, 2, 3))
+
+
+/**
+ * @description 有一个整型数组，从数组中取出两个最大的值，用大值减去小值，若果结果不为 0，则将结果放回数组，重复上面操作，直到数组中只有一个元素或为空，若只有一个元素，则返回该元素，若为空，则返回 0
+ * @param {*} arr 
+ */
+function max(arr) {
+    if (arr.length === 0) {
+        return 0;
+    } else if (arr.length === 1) {
+        return arr[0];
+    } else {
+        let max1 = Math.max(...arr);
+        arr.splice(find(arr, max1), 1)
+        let max2 = Math.max(...arr);
+        arr.splice(find(arr, max2), 1)
+        if (max1 - max2 != 0) {
+            arr.push(max1 - max2);
+        }
+        console.log(arr)
+        return max(arr)
+
+    }
+}
+
+function find(arr, num) {
+    return arr.findIndex((item) => {
+        return item == num
+    })
+}
+// console.log(max([1, 7, 9, 15, 44]))
