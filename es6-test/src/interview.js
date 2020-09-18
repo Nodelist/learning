b8e21d300c1f18022137b8e19202baf21029a41b
wq <2359188347@qq.com>
2020-09-18 18:01:54 +0800

    更新

--- es6-test/src/interview.js
+++ es6-test/src/interview.js
@@ -7,38 +7,24 @@ http.createServer((request, response) => {
     response.end("Hello World!")
 }).listen(9091)
 
-for (let i = 0; i < 5; i++) {
-    setTimeout(function() {
-        console.log(new Date, i, 1)
-    }, 1000)
-    console.log(new Date, i)
-}
-/*
-    * 以上输出
-    Tue Sep 15 2020 10:16:01 GMT+0800 (GMT+08:00) 0
-    Tue Sep 15 2020 10:16:01 GMT+0800 (GMT+08:00) 1
-    Tue Sep 15 2020 10:16:01 GMT+0800 (GMT+08:00) 2
-    Tue Sep 15 2020 10:16:01 GMT+0800 (GMT+08:00) 3
-    Tue Sep 15 2020 10:16:01 GMT+0800 (GMT+08:00) 4
-    Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 0 1
-    Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 1 1
-    Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 2 1
-    Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 3 1
-    Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 4 1
-*/
-
-console.log(+true); // 1
-console.log(!'str'); // false
-let a = [1, 2, 3];
-let b = [1, 2, 3];
-let c = b;
-console.log(c[0])
-console.log(a == b); // false
-console.log(a === b); // false
-console.log(b == c) // true
-console.log(b === c) // true
-console.log(NaN == NaN); // false
-console.log(NaN != NaN); // true
+// for (let i = 0; i < 5; i++) {
+//     setTimeout(function() {
+//         console.log(new Date, i, 1)
+//     }, 1000)
+//     console.log(new Date, i)
+// }
+// console.log(+true); // 1
+// console.log(!'str'); // false
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+// let c = b;
+// console.log(c[0])
+// console.log(a == b); // false
+// console.log(a === b); // false
+// console.log(b == c) // true
+// console.log(b === c) // true
+// console.log(NaN == NaN); // false
+// console.log(NaN != NaN); // true
 /*
     * 以上输出
     Tue Sep 15 2020 10:16:01 GMT+0800 (GMT+08:00) 0
@@ -58,21 +44,21 @@ console.log(NaN != NaN); // true
     Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 3 1
     Tue Sep 15 2020 10:16:02 GMT+0800 (GMT+08:00) 4 1
 */
-let A = { a: 1, b: 2 };
-let B = A;
-B.b = "str";
-console.log(A); // {a: 1, b: 'str'}
-console.log(B); // {a: 1, b: 'str'}
+// let A = { a: 1, b: 2 };
+// let B = A;
+// B.b = "str";
+// console.log(A); // {a: 1, b: 'str'}
+// console.log(B); // {a: 1, b: 'str'}
 
-let arr = [1, 2, 3];
-console.log(...arr) // 1 2 3 (序列)
-let arrb = [...arr, 4, 5];
-console.log(arrb) // [1,2,3,4,5] (数组)
+// let arr = [1, 2, 3];
+// console.log(...arr) // 1 2 3 (序列)
+// let arrb = [...arr, 4, 5];
+// console.log(arrb) // [1,2,3,4,5] (数组)
 
-console.log(1 + true) // 2
-console.log(1 + false) // 1
-console.log('' + true) // true
-console.log('' + false) // false
+// console.log(1 + true) // 2
+// console.log(1 + false) // 1
+// console.log('' + true) // true
+// console.log('' + false) // false
 
 
 /**
@@ -123,3 +109,64 @@ function find(arr, num) {
     })
 }
 // console.log(max([1, 7, 9, 15, 44]))
+
+
+/**
+ * @description 32位有符号证整数反转，溢出返回 0
+ * @param {*} params 
+ */
+function reverse(params) {
+    debugger
+    var arr = Math.abs(params).toString().split("")
+    arr.reverse();
+    var a = parseInt(arr.join(''))
+    if (params > 0) {
+        return a > 2 ** 31 - 1 ? 0 : a
+    } else {
+        return a > 2 ** 31 ? 0 : -a
+    }
+}
+// console.log(reverse(1534236469))
+// console.log(reverse(123))
+// console.log(reverse(-123))
+
+/**
+ * @description 全排序
+ * @param {Array} params 
+ */
+function squence(params) {
+    let len = params.length;
+    let used = [params.length];
+    params.sort((a, b) => a - b)
+    for (let i = 0; i < len; i++) {
+
+    }
+}
+
+
+/**
+ * @description 判断是不是回文数
+ * @param {Number} params 
+ */
+function palindrome(params) {
+    // 解法一
+    // let arr = params.toString().split("")
+    // let len = arr.length
+    // for (let i = 0; i < Math.floor(len / 2); i++) {
+    //     if (arr[i] == arr[len - i - 1]) {
+    //         continue;
+    //     } else {
+    //         return false
+    //     }
+    // }
+    // return true
+
+    // 解法二
+    let newStr = params.toString().split("").reverse().join("");
+    if (newStr === params.toString()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(palindrome(1031))
