65d250f841e20e4bf3e77998bfa578b3d8a462cb
wq <2359188347@qq.com>
2020-09-30 11:26:14 +0800

    提交

--- vue-router/src/components/introduction.vue
+++ vue-router/src/components/introduction.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     介绍
+    {{ name }}
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+  },
+
+  created() {
+    debugger;
+    console.log(this.name);
+  },
+};
 </script>
 
 <style></style>
