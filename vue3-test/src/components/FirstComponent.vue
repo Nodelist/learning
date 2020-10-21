ad9046e8bea22daa4f89a791a02208a44bba6217
Nodelist <2359188347@qq.com>
2020-10-21 11:22:01 +0800

    更新

---
+++ vue3-test/src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        FirtChild
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FirtChild',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
